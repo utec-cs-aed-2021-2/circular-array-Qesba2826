661d2b9212c25c2e342b3f6b21ca35985ed9769d
Qes <quespisisa.bustos@utec.edu.pe>
2021-09-13 13:10:25 -0500

    Primer cambio

--- circulararray.h
+++ circulararray.h
@@ -13,15 +13,15 @@ public:
     CircularArray();
     CircularArray(int _capacity);
     virtual ~CircularArray();
-    void push_front(T data);
-    void push_back(T data);
-    void insert(T data, int pos);
-    T pop_front();
-    T pop_back();
-    bool is_full();
-    bool is_empty();
+    void push_front(T data);  //Hecho
+    void push_back(T data);  //Hecho
+    void insert(T data, int pos);  //Hecho
+    T pop_front();  //Hecho
+    T pop_back();  //Hecho
+    bool is_full(); //Hecho
+    bool is_empty(); //Hecho
     int size();
-    void clear();
+    void clear(); //Hecho
     T &operator[](int);
     void sort();
     bool is_sorted();
@@ -65,11 +65,68 @@ int CircularArray<T>::next(int index)
     return (index + 1) % capacity;
 }
 
-template <class T>
+/*template <class T>
 string CircularArray<T>::to_string(string sep)
 {
     string result = ""; 
     for (int i = 0; i < size(); i++)
         result += std::to_string((*this)[i]) + sep;
     return result;    
+}*/
+
+
+template<class T>
+void CircularArray<T>::push_front(T data)
+{
+    array[next(front)] = data;
+    front++;
+    return;
+}
+
+template<class T>
+void CircularArray<T>::push_back(T data)
+{
+    array[prev(back)] = data;
+    back--;
+    return;
+}
+
+/*template<class T>
+void CircularArray<T>::insert(T data, int pos){
+    array[pos]=data;
+    return;
+}*/
+
+template<class T>
+T CircularArray<T>::pop_front(){
+    array[front] = 0;
+    return front--;
+}
+
+template<class T>
+T CircularArray<T>::pop_back(){
+    array[back] = 0;
+    return back++;
+}
+
+template<class T>
+bool CircularArray<T>::is_empty(){
+    return (front == -1 && back ==-1);
+}
+
+template<class T>
+bool CircularArray<T>::is_full(){
+    return (back = next(front));
+}
+
+template<class T> 
+void CircularArray<T>::clear(){
+    delete array;
+    front = -1;
+    back = -1;
+}
+
+template<class T>
+int CircularArray<T>::size(){
+    
 }
