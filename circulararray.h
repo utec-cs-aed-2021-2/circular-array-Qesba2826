7fadadf4588be0c75ddf71a87c0c79f99ceb1e0e
Qes <quespisisa.bustos@utec.edu.pe>
2021-09-13 21:39:19 -0500

    Segundo cambio

--- circulararray.h
+++ circulararray.h
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
 using namespace std;
 
 template <class T>
@@ -20,7 +23,7 @@ public:
     T pop_back();  //Hecho
     bool is_full(); //Hecho
     bool is_empty(); //Hecho
-    int size();
+    int size(); //Hecho
     void clear(); //Hecho
     T &operator[](int);
     void sort();
@@ -56,77 +59,114 @@ CircularArray<T>::~CircularArray()
 template <class T>
 int CircularArray<T>::prev(int index)
 {
-    return (index == 0) ? capacity - 1 : index - 1;
+    return (index <= 0) ? capacity - 1 : index - 1;
 }
 
 template <class T>
 int CircularArray<T>::next(int index)
 {
-    return (index + 1) % capacity;
+    return  (index + 1) % capacity;
 }
 
-/*template <class T>
+template <class T>
 string CircularArray<T>::to_string(string sep)
 {
     string result = ""; 
-    for (int i = 0; i < size(); i++)
+    for (int i = 0; i < size()+1; i++)
         result += std::to_string((*this)[i]) + sep;
     return result;    
-}*/
+}
 
+template<class T>
+bool CircularArray<T>::is_empty(){
+    return (front == back);
+}
+
+template<class T>
+bool CircularArray<T>::is_full(){
+    return (size() == capacity);
+}
 
 template<class T>
 void CircularArray<T>::push_front(T data)
 {
-    array[next(front)] = data;
-    front++;
+    if (is_full() == false){
+        front=next(front);
+        *(array+front) = data;
+    }
+    else {
+        cout<<"Array is full can not push more data"<<endl;
+    }
+    cout<<"front:"<<front<<" data:"<<data<<endl;
+    cout<<*(array+front)<<endl;
     return;
 }
 
 template<class T>
 void CircularArray<T>::push_back(T data)
 {
-    array[prev(back)] = data;
-    back--;
+    if (is_full() == false){
+        back=prev(back);
+        *(array+back)=data;
+    }
+    else {
+        cout<<"Array is full can not push more data."<<endl;
+    }
+    cout<<"back:"<<back<<" data:"<<data<<endl;
+    cout<<*(array+back)<<endl;
     return;
 }
 
-/*template<class T>
-void CircularArray<T>::insert(T data, int pos){
-    array[pos]=data;
+template<class T>
+void CircularArray<T>::insert(T data, int pos)
+{
+    *(array+pos)=data;
     return;
-}*/
+}
 
 template<class T>
-T CircularArray<T>::pop_front(){
-    array[front] = 0;
-    return front--;
+T CircularArray<T>::pop_front()
+{
+    cout<<"frontorg"<<front<<endl;
+    front=(front+capacity)%capacity;
+    if (is_empty() == false){
+        *(array+front) = 0;
+        front=prev(front);
+        cout<<"front:"<<front<<" valuein: "<<*(array+front)<<endl;
+        }
+    else{
+        cout<<"Array is empty can not pop more data"<<endl;
+        }
+    cout<<"front:"<<front<<" value: "<<*(array+front)<<endl;
+    return front;
 }
 
 template<class T>
 T CircularArray<T>::pop_back(){
-    array[back] = 0;
-    return back++;
-}
-
-template<class T>
-bool CircularArray<T>::is_empty(){
-    return (front == -1 && back ==-1);
-}
-
-template<class T>
-bool CircularArray<T>::is_full(){
-    return (back = next(front));
+    if (is_empty() == false){
+        *(array + back) = back*0;
+        back=next(back);
+        }
+    else{
+        cout<<"Array is empty can not pop more data."<<endl;
+        }
+    cout<<"back:"<<back<<" value: "<<*(array+back)<<endl;
+    return back;
 }
 
 template<class T> 
 void CircularArray<T>::clear(){
     delete array;
     front = -1;
-    back = -1;
+    back = -1;   
 }
 
 template<class T>
 int CircularArray<T>::size(){
-    
+    return (capacity + front - back)%capacity;
+}
+
+template<class T>
+T &CircularArray<T>::operator[](int front){
+    return array[front];
 }
